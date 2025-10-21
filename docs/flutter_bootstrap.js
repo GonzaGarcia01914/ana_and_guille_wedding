5b7e491152ae0ab64210b3ab7ca5990967a4a620
GonzaGarcia01914 <gonzalogarcia01914@gmail>
2025-10-22 00:20:17 +0200

    version_1.0.2

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -36,8 +36,4 @@ if (!window._flutter) {
 }
 _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a72","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
-_flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "212860755"
-  }
-});
+_flutter.loader.load();
